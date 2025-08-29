7305669e3829e6b7549e5c040debda8ac927e4a2
luismelo <felipe_melo_1@hotmail.com>
2025-08-28 21:54:13 -0300

    initial commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import AppNavigation from './src/navigation/index';
+import Toast from 'react-native-toast-message';
+
+export default function App() {
+  return (
+    <>
+      <AppNavigation />
+      <Toast />
+    </>
+  );
+}
